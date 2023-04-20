9a243eb857d4e06c442ac71fb13bab9f7ef12314
Csaba000 <kormoci.csaba@student.ms.sapientia.ro>
2023-04-20 19:39:53 +0300

    Changed poolAddress name

--- mobile-app/tests/getReserves.test.js
+++ mobile-app/tests/getReserves.test.js
@@ -3,17 +3,18 @@ import abiPool from "../contract/abiPool.json";
 
 describe("getReserves function", () => {
   let contract;
+  
   const token1Reserve = "1014334013426664948"; // 1.014334013426672082
   const token2Reserve = "141733722092157"; // 0.000141733722072185
+
+  const poolAddress = "0x9D67D3969e45aDf9786F95A1F874C0E8d476630F";
 
   beforeAll(async () => {
     const AlchemyProvider = new ethers.providers.JsonRpcProvider(
       "https://eth-goerli.g.alchemy.com/v2/bnqXJ3kma1bUnhQmSTmGYzp96ch3kpVh"
     );
 
-    const contractAddress = "0x9D67D3969e45aDf9786F95A1F874C0E8d476630F";
-
-    contract = new ethers.Contract(contractAddress, abiPool, AlchemyProvider);
+    contract = new ethers.Contract(poolAddress, abiPool, AlchemyProvider);
   });
 
   it("should return reserves", async () => {
